e5b8e7db899eeb639ab1694007a1f094d1acf6e7
tamasbanyasz <116582308+tamasbanyasz@users.noreply.github.com>
2022-11-18 19:29:21 +0100

    Rock-Paper-Scissors
    With magic methods and pie chart

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,4 +1,41 @@
+import matplotlib.pyplot as plt
 import numpy as np
+
+
+class ResultVisualization:
+    def __init__(self, players_name, player_res):
+        super().__init__()
+        self.players_name = players_name
+        self.players_result = player_res
+        self.colors = ("red", "green", "brown")
+
+        self.index_of_max_result = np.argmax(self.players_result)
+        self.explode_values_in_array = np.array([0.] * len(self.players_result))
+        self.explode_values_in_array[self.index_of_max_result] = 0.3
+
+        self.explode = tuple(self.explode_values_in_array)
+
+        self.wp = {'linewidth': 2, 'edgecolor': "black"}
+
+        self.fig, self.ax = plt.subplots(figsize=(10, 7))
+
+        self.wedges, self.tx, self.autotexts = plt.pie(self.players_result, labels=self.players_name,
+                                                       explode=self.explode, autopct="",
+                                                       wedgeprops=self.wp, shadow=True, colors=self.colors)
+
+        for i, a in enumerate(self.autotexts):
+            a.set_text("{}".format(self.players_result[i]))
+
+        self.ax.legend(self.wedges, self.players_name,
+                       title="Players name",
+                       loc="upper right",
+                       fontsize=16,
+                       bbox_to_anchor=(0.8, 0, 0.5, 1))
+
+        plt.setp(self.autotexts, size=22, weight="bold")
+
+        self.ax.set_title("Rock-Paper-Scissors Game")
+        plt.show()
 
 
 class Player:
@@ -20,7 +57,7 @@ class Player:
         return not self.__eq__(other)
 
     def __repr__(self):
-        return f'{self.name}: {self.win}'
+        return f'{self.name}'
 
 
 class Battle:
@@ -38,9 +75,6 @@ class Battle:
             self.won = 'Paper'
 
         return self.won
-
-    def __str__(self):
-        return f'\n{self.won} has won the battle'
 
 
 class Game:
@@ -61,24 +95,23 @@ class Game:
 
     def end_of_the_game(self):
         for player in self.players:
-            if player == 10000:
+            if player == 1000:
+                print(f'\n{player} has won the battle')
                 return True
         return False
 
-    def display_game(self):
-        self.add_win_number_to_player()
-        if self.end_of_the_game():
-            return True
-        return False
+    def game_loop(self):
+        while True:
+            self.add_win_number_to_player()
+            if self.end_of_the_game():
+                print(f'\n{self.rock.name}: {self.rock.win}\n{self.paper.name}: {self.paper.win}\n{self.scissors.name}:'
+                      f' {self.scissors.win}')
+                break
 
-    def __str__(self):
-        return f'\n{self.rock}\n{self.paper}\n{self.scissors}\n{self.battle}'
+    def display_pie_chart(self):
+        self.game_loop()
+        ResultVisualization(self.players, np.array([self.rock.win, self.scissors.win, self.paper.win]))
 
 
 game = Game()
-while True:
-    if game.display_game():
-        print(game)
-        break
-
-print(vars(game))
+game.display_pie_chart()

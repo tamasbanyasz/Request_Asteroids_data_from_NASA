c30d146f63d1bd94a5b9bbd70d2e0765a5b127fe
tamasbanyasz <116582308+tamasbanyasz@users.noreply.github.com>
2022-12-07 22:33:46 +0100

    Create SQL table for asteroids data

--- Pysql.py
+++ Pysql.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Column
 from sqlalchemy import Integer
-from sqlalchemy import VARCHAR
+from sqlalchemy import String
 from sqlalchemy import Float
+from sqlalchemy import BOOLEAN
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -11,15 +12,17 @@ class Asteroids(Base):
     __tablename__ = "asteroids"
 
     id = Column(Integer, primary_key=True)
-    asteroids_name = Column(VARCHAR(50))
-    asteroids_close_date = Column(VARCHAR(20))
+    asteroids_name = Column(String(50))
+    close_approach_date_full = Column(String(20))
     asteroids_miss_distance_in_km = Column(Float)
-    velocitys_in_km_s = Column(Float)
+    relative_velocity_in_km_s = Column(Float)
     asteroids_estimated_diameter_min_in_m = Column(Float)
+    asteroid_is_potentially_dangerous = Column(BOOLEAN)
 
     def __repr__(self):
         return f"Asteroid(id={self.id!r}, asteroid_name={self.asteroids_name!r}, " \
-               f"close_date={self.asteroids_close_date!r}," \
+               f"close_approach_date_full={self.close_approach_date_full!r}," \
                f"miss_distance_in_km={self.asteroids_miss_distance_in_km!r}, " \
-               f"velocitys_in_km/s={self.velocitys_in_km_s!r}," \
-               f"estimated_diameter_min_inm={self.asteroids_estimated_diameter_min_in_m!r})"
+               f"relative_velocity_in_km/s={self.relative_velocity_in_km_s!r}," \
+               f"estimated_diameter_min_in_m={self.asteroids_estimated_diameter_min_in_m!r}," \
+               f"asteroid_is_potentially_dangerous={self.asteroid_is_potentially_dangerous!r})"

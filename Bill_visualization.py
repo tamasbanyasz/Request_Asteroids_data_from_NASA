9ba0ce8037de017a94d497ebf0d2e8675d6cf0f3
tamasbanyasz <116582308+tamasbanyasz@users.noreply.github.com>
2022-12-07 22:17:47 +0100

    Bill visualization
    Bill visualization from JSON file with GUI

--- Bill_visualization.py
+++ Bill_visualization.py
@@ -1,4 +1,3 @@
-
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
@@ -11,9 +10,9 @@ import orjson
 
 class Visualization:
     def __init__(self, df, title):
-        self.fig = px.line(df, x=df.Years, y=df.columns, hover_data={"Years": "|#%B,%Y"},
-                           title=title, labels={"variable": "Bills", "value": "Amount", "Years": "Date", })
-
+        self.fig = px.line(df, x=df.Years, y=df.columns, hover_data={"Years": "|%B, %Y"},
+                           title=title, labels={"variable": "Bills", "value": "Amount", "Years": "Date"})
+        self.fig.update_traces(mode="markers+lines")
         self.fig.update_xaxes(
             dtick="M1",
             tickformat="%b\n%Y"
@@ -23,38 +22,39 @@ class Visualization:
 
 
 class JsonFunctions:
-    def __init__(self):
-        pass
+    def __init__(self, file_name):
+        self.file_name = file_name
 
-    @staticmethod
-    def read_json():
-        with open("bills.json", "rb") as f:
+    def write_to_json(self, new_jsonfile):
+        with open(f"{self.file_name}.json", "wb") as f:
+            f.write(orjson.dumps(new_jsonfile, option=orjson.OPT_INDENT_2))
+
+    def read_json(self):
+        with open(f"{self.file_name}.json", "rb") as f:
             json_data = orjson.loads(f.read())
 
             return json_data
 
-    @staticmethod
-    def json_exists():
-        file_exists = os.path.exists('bills.json')
+    def json_exists(self):
+        file_exists = os.path.exists(f"{self.file_name}.json")
         if file_exists:
             pass
         else:
             test_dict = {
-                'Years': ['2021-01', '2021-02', '2021-03', '2021-04', '2021-05', '2021-06', '2021-07', '2021-08',
-                          '2021-09', '2021-10',
-                          '2021-11', '2021-12', '2022-01', '2022-02', '2022-03', '2022-04', '2022-05', '2022-06',
-                          '2022-07', '2022-08', '2022-09', '2022-10'],
-                'Water': [2000, 1000, 250, 770, 330, 920, 230, 550, 1200, 1500, 204, 2800, 2008, 800, 150, 205, 340,
-                          400, 501, 600, 800, 500],
+                'Years': ['2021-01', '2021-01', '2021-02', '2021-03', '2021-04', '2021-04', '2021-05', '2021-06',
+                          '2021-07', '2021-08', '2021-09', '2021-10',
+                          '2021-11', '2021-12', '2022-01', '2022-01', '2022-02', '2022-03', '2022-04', '2022-04',
+                          '2022-05', '2022-06', '2022-07', '2022-08', '2022-09', '2022-10', '2022-11'],
+                'Water': [2000, 1467, 1000, 250, 770, 330, 601, 920, 230, 550, 1200, 1500, 204, 2800, 2008, 800, 150,
+                          205, 340, 400, 501, 600, 800, 500, 386, 777, 1300],
 
-                'Electricity': [3000, 2000, 1500, 1100, 800, 700, 600, 800, 900, 1000, 1100, 3060, 300, 500, 600, 900,
-                                850, 320, 310, 450, 550, 600],
-                'Gas': [4000, 800, 401, 401, 450, 401, 400, 403, 450, 477, 402, 400, 490, 403, 440, 407, 401, 420, 410,
-                        500, 1000, 1255]
-                }
+                'Electricity': [3000, 1777, 2000, 1500, 1100, 800, 501, 700, 600, 800, 900, 1000, 1100, 3060, 300, 500,
+                                600, 900, 850, 320, 310, 450, 550, 600, 999, 1300, 1000],
+                'Gas': [4000, 2100, 800, 401, 401, 450, 301, 401, 400, 403, 450, 477, 402, 400, 490, 403, 440, 407, 401,
+                        420, 410, 500, 1000, 1255, 1500, 1790, 1100]
+            }
 
-            with open("bills.json", "wb") as f:
-                f.write(orjson.dumps(test_dict))
+            self.write_to_json(test_dict)
 
         return file_exists
 
@@ -142,9 +142,10 @@ class DateToDateBills:
         self.date_to_date = DatetToDatesLabel(tk_window)
 
     def selected_dates(self, jsonfile):
+        jsonfile = pd.DataFrame(jsonfile)
         start_date = self.date_to_date.get_start_date()
         end_date = self.date_to_date.get_end_date()
-        years = [i for i in jsonfile['Years']]
+        years = set(i for i in jsonfile['Years'])
 
         if not years.__contains__(start_date and end_date):
             messagebox.showerror("Invalid", "There is no such date.")
@@ -153,22 +154,23 @@ class DateToDateBills:
         start_date_index = jsonfile.index[jsonfile['Years'] == start_date]
         end_date_index = jsonfile.index[jsonfile['Years'] == end_date]
 
-        if start_date_index > end_date_index:
+        if start_date_index[0] > end_date_index[-1:][0]:
             messagebox.showerror("Invalid", "Incorrect date format.")
             return window.mainloop()
 
-        selected_bills = jsonfile[start_date_index[0]:end_date_index[0] + 1]
+        selected_bills = jsonfile[start_date_index[0]:end_date_index[-1:][0] + 1]
         Visualization(selected_bills, "Date to date")
 
 
-class SubmitAmounts:
-    def __init__(self, tk_window):
+class SubmitAmounts(JsonFunctions):
+    def __init__(self, tk_window, file_name):
+        super().__init__(file_name)
         self.water = WaterLabel(tk_window)
         self.electricity = ElectricityLabel(tk_window)
         self.gas = GasLabel(tk_window)
         self.date_now = datetime.now().strftime("%Y-%m")
 
-    def send_amounts(self):
+    def send_amounts(self, jsonfile):
         water = self.water.get_water_amount()
         electricity = self.electricity.get_electricity_amount()
         gas = self.gas.get_gas_amount()
@@ -177,29 +179,32 @@ class SubmitAmounts:
             messagebox.showerror("Invalid", "Incorrect input.")
             return window.mainloop()
 
-        amounts = [self.date_now, int(water), int(electricity), int(gas)]
-        print(amounts)
-        print("Not working yet.")
+        jsonfile['Years'].append(self.date_now)
+        jsonfile['Water'].append(int(water))
+        jsonfile['Electricity'].append(int(electricity))
+        jsonfile['Gas'].append(int(gas))
+
+        self.write_to_json(jsonfile)
+        messagebox.showinfo("Information", "Amounts sent.")
 
 
 class YearlyBills:
     def __init__(self, jsonfile):
         self.index = []
+        self.jsonfile = pd.DataFrame(jsonfile)
 
-        for index, value in enumerate(jsonfile['Years']):
+        for index, value in enumerate(self.jsonfile['Years']):
             if datetime.now().strftime("%Y") in value:
                 self.index.append(index)
 
-        self.yearly_bills = jsonfile.loc[self.index[0]:self.index[-1]]
+        self.yearly_bills = self.jsonfile.loc[self.index[0]:self.index[-1]]
 
         Visualization(self.yearly_bills, "Yearly bills")
 
 
 class AllBills:
     def __init__(self, jsonfile):
-        pass
-
-        Visualization(jsonfile, "All Bills")
+        Visualization(pd.DataFrame(jsonfile), "All Bills")
 
 
 class DateToDatesButton:
@@ -223,7 +228,6 @@ class AllBillsButton:
 
 class YearlyButton:
     def __init__(self, tk_window, jsonfile):
-
         self.yearly_button = tk.Button(tk_window, text='Yearly',
                                        command=lambda: YearlyBills(jsonfile), font=('Arial', 16))
 
@@ -231,24 +235,27 @@ class YearlyButton:
 
 
 class SubmitButton:
-    def __init__(self, tk_window):
-        self.s = SubmitAmounts(tk_window)
-        self.submit_button = tk.Button(tk_window, text='Submit', command=lambda: self.s.send_amounts(),
+    def __init__(self, tk_window, jsonfile, name_of_json_file):
+        self.s = SubmitAmounts(tk_window, name_of_json_file)
+
+        self.submit_button = tk.Button(tk_window, text='Submit',
+                                       command=lambda: self.s.send_amounts(jsonfile),
                                        font=('Arial', 16))
 
         self.submit_button.place(x=210, y=220)
 
+
+json_file_name = "bills"
 
 window = tk.Tk()
 window.geometry("800x600")
 window.title("Bányász Tamás")
 window.resizable(False, False)
 
-JsonFunctions().json_exists()
+json_functions = JsonFunctions(json_file_name)
+json_functions.json_exists()
 
-json_obj = JsonFunctions().read_json()
-json_object = pd.DataFrame(json_obj)
-
+datas_in_dict = json_functions.read_json()
 
 BillsLabel(window)
 WaterLabel(window)
@@ -256,9 +263,9 @@ ElectricityLabel(window)
 GasLabel(window)
 DatetToDatesLabel(window)
 
-DateToDatesButton(window, json_object)
-SubmitButton(window)
-YearlyButton(window, json_object)
-AllBillsButton(window, json_object)
+DateToDatesButton(window, datas_in_dict)
+YearlyButton(window, datas_in_dict)
+AllBillsButton(window, datas_in_dict)
+SubmitButton(window, datas_in_dict, json_file_name)
 
 window.mainloop()

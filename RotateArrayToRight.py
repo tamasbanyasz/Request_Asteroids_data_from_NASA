25262d4ec6c72049a0d51b112255ad99fb102ddc
tamasbanyasz <116582308+tamasbanyasz@users.noreply.github.com>
2023-02-28 14:06:18 +0100

    Rotate arrays

--- RotateArrayToRight.py
+++ RotateArrayToRight.py
@@ -1,19 +1,60 @@
-from numpy import array, concatenate
+
+class Line:
+
+    def __init__(self, value):
+        self.line = self.get_parameter(value)
+        self.line_length = len(self.line)
+
+    def get_array(self):
+        return self.line
+
+    def get_parameter(self, value):
+        return self.generate_array_if_value_is_integer(value) or self.value_can_be_slicing(value)
+
+    def generate_array_if_value_is_integer(self, value):
+        if type(value) == int:
+            return [i for i in range(value)]
+
+    def value_can_be_slicing(self, value):
+        if type(value) != int:
+            return [i for i in value]
+
+    def rotate_right_one_by_one(self, my_array):
+        last_item_of_my_array = my_array.pop()
+        my_array.insert(0, last_item_of_my_array)
+
+        return my_array
+
+    def rotate_left_one_by_one(self, my_array):
+        first_item_of_my_array = my_array.pop(0)
+        my_array.insert(self.line_length, first_item_of_my_array)
+
+        return my_array
+
+    def rotate_right_by_a_specific_number(self, my_array, selected_number):
+        for i in range(selected_number):
+            self.rotate_right_one_by_one(my_array)
+
+        return my_array
+
+    def rotate_left_by_a_specific_number(self, my_array, selected_number):
+        for i in range(selected_number):
+            self.rotate_left_one_by_one(my_array)
+
+        return my_array
 
 
-class ArrayRotate:
-    def __init__(self, length, number_of_rotate):
-        self.array_length = length
-        self.k = number_of_rotate
-        self.mylist = array([item for item in range(1, self.array_length+1)])
+list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+number = 10
+string = "Hello"
 
-    def to_right(self):
-        print(f'Original array --> {self.mylist}')
-        return concatenate([self.mylist[self.array_length - self.k:self.array_length], self.mylist[self.array_length - self.array_length:self.array_length - self.k]])
+first_line = Line(string)
 
+first_array = first_line.get_array()
+print(f'\nOriginal array -- > {first_array}')
 
-array_rotate = ArrayRotate(10, 1)
+rotated_by_one = first_line.rotate_right_one_by_one(first_array)
+print(f'\nRotated to right by one -- >{rotated_by_one}')
 
-rotated_array = array_rotate.to_right()
-
-print(f'Rotated array --> {rotated_array}')
+rotated_left_by_a_specific_index = first_line.rotate_left_by_a_specific_number(rotated_by_one, 3)
+print(f'\nRotated to left by a specific index -- > {rotated_left_by_a_specific_index}')
